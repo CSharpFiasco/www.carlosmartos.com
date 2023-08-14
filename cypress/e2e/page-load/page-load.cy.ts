cf9cbef63760e2c47b9469352732ce5b26858a62
Carlos Martos <40156600+CSharpFiasco@users.noreply.github.com>
2023-08-13 20:20:30 -0500

    Formatting change plus add component test (#3)

--- cypress/e2e/page-load/page-load.cy.ts
+++ cypress/e2e/page-load/page-load.cy.ts
@@ -1,16 +1,14 @@
 let baseUrl: string | undefined;
 
 describe('home page', () => {
-    
+  beforeEach(() => {
+    baseUrl = Cypress.env('base_url');
+    if (typeof baseUrl !== 'string') throw new Error('base_url is not a string');
 
-    beforeEach(() => {
-        baseUrl = Cypress.env('base_url');
-        if(typeof baseUrl !== 'string') throw new Error('base_url is not a string');
+    cy.visit(baseUrl);
+  });
 
-        cy.visit(baseUrl);
-    });
-
-    it('should have a greeting text', () => {
-        cy.get('.greeting-text').should('contain.text', 'Carlos');
-    });
+  it('should have a greeting text', () => {
+    cy.get('.greeting-text').should('contain.text', 'Carlos');
+  });
 });
---
+++ src/App.test.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { describe, expect, it, vi } from 'vitest';
+import { render } from '@testing-library/react';
+import App from './App';
+
+const MockIntersectionObserver = vi.fn(() => ({
+  disconnect: vi.fn(),
+  observe: vi.fn(),
+  takeRecords: vi.fn(),
+  unobserve: vi.fn(),
+}));
+vi.stubGlobal(`IntersectionObserver`, MockIntersectionObserver);
+
+describe('App', () => {
+  it('renders without crashing', () => {
+    const app = render(<App />);
+    expect(app).toBeTruthy();
+  });
+});
