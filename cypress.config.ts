22473e160761a44317d8331494014c0bf0d5378c
CSharpFiasco <40156600+CSharpFiasco@users.noreply.github.com>
2024-01-22 20:39:26 -0600

    Refactor to use scss mixin for font resizing, adjust readme

--- cypress.config.ts
+++ cypress.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
         log(message) {
           // Then to see the log messages in the terminal
           //   cy.task("log", "my message");
-          console.log(message +'\n\n');
+          console.log(message + '\n\n');
           return null;
         },
       });
