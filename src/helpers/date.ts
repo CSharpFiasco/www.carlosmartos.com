db4c56a472ba484599ab968425415604814965e8
Carlos Martos <40156600+CSharpFiasco@users.noreply.github.com>
2024-04-28 21:23:26 -0500

    remove hard-coded value (#36)

---
+++ src/helpers/date.ts
@@ -0,0 +1,9 @@
+/**
+ * @param fromDate 
+ * @returns years since date, rounded to nearest integer
+ */
+export const yearsSinceDate = (fromDate: Date) => {
+    const millisecondsInYear = 1000 * 60 * 60 * 24 * 365;
+
+    return Math.round((Date.now() - fromDate.getTime()) / millisecondsInYear);
+}
