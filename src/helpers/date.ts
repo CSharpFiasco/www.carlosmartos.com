027605acb3efe6e62f77cd45653002a1d5c1d8e9
Carlos Martos <40156600+CSharpFiasco@users.noreply.github.com>
2024-06-22 21:57:48 -0500

    Run prettier

--- src/helpers/date.ts
+++ src/helpers/date.ts
@@ -1,9 +1,9 @@
 /**
- * @param fromDate 
+ * @param fromDate
  * @returns years since date, rounded to nearest integer
  */
 export const yearsSinceDate = (fromDate: Date) => {
-    const millisecondsInYear = 1000 * 60 * 60 * 24 * 365;
+  const millisecondsInYear = 1000 * 60 * 60 * 24 * 365;
 
-    return Math.round((Date.now() - fromDate.getTime()) / millisecondsInYear);
-}
+  return Math.round((Date.now() - fromDate.getTime()) / millisecondsInYear);
+};
