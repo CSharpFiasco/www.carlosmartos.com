3b1429a3bc71a38b1f484f9308b076adc18013ae
Carlos Martos <40156600+CSharpFiasco@users.noreply.github.com>
2025-08-17 18:07:27 -0500

    migrate from astro to angular

--- src/helpers/date.ts
+++
@@ -1,9 +0,0 @@
-/**
- * @param fromDate
- * @returns years since date, rounded to nearest integer
- */
-export const yearsSinceDate = (fromDate: Date) => {
-  const millisecondsInYear = 1000 * 60 * 60 * 24 * 365;
-
-  return Math.round((Date.now() - fromDate.getTime()) / millisecondsInYear);
-};
