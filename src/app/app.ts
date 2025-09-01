cc340471670e71f6b51807dd1a1e1be650698be6
Carlos Martos <40156600+CSharpFiasco@users.noreply.github.com>
2025-09-01 12:18:55 -0500

    move components to feature folder

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Greeting } from "../greeting/greeting";
-import { Skills } from "../skills/skills";
-import { Education } from "../education/education";
-import { WorkExperience } from "../work-experience/work-experience";
-import { Achievement } from "../achievement/achievement";
+import { Greeting } from "../features/greeting/greeting";
+import { Skills } from "../features/skills/skills";
+import { Education } from "../features/education/education";
+import { WorkExperience } from "../features/work-experience/work-experience";
+import { Achievement } from "../features/achievement/achievement";
 
 @Component({
   selector: 'app-root',
