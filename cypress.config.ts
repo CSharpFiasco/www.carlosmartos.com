7b8b13530e16eb24277fc3f4fcf2e7d693a11856
Carlos Martos <40156600+CSharpFiasco@users.noreply.github.com>
2025-09-06 15:41:25 -0500

    add back cypress and workflows

---
+++ cypress.config.ts
@@ -0,0 +1,17 @@
+import { defineConfig } from 'cypress';
+
+export default defineConfig({
+  e2e: {
+    video: false,
+    setupNodeEvents(on, _config) {
+      on('task', {
+        log(message) {
+          // Then to see the log messages in the terminal
+          //   cy.task("log", "my message");
+          console.log(message + '\n\n');
+          return null;
+        },
+      });
+    },
+  },
+});
---
+++ cypress/e2e/page-load/page-load.cy.ts
@@ -0,0 +1,40 @@
+/// <reference types="cypress" />
+let baseUrl: string | undefined;
+
+describe('home page', () => {
+  beforeEach(() => {
+    baseUrl = Cypress.env('base_url');
+    if (typeof baseUrl !== 'string') throw new Error('base_url is not a string');
+
+    cy.visit(baseUrl);
+  });
+
+  it('should have expected elements', () => {
+    cy.get('#greeting').should('contain.text', 'Carlos');
+  });
+
+  it('should link to resume', () => {
+    cy.get('#resume-download-button')
+      .should('be.visible')
+      .should('have.attr', 'target', '_blank')
+      .should(($a) => {
+        expect($a.attr('href')).contains('pdf');
+      })
+      .click();
+  });
+
+  it('should have valid links', () => {
+    cy.get('a').each(($a) => {
+      const href = $a.prop('href');
+
+      cy.request({
+        url: href,
+        failOnStatusCode: false,
+      })
+        .its('status')
+        .should((status) => {
+          expect(status).to.be.oneOf([200, 418, 403, 429]);
+        });
+    });
+  });
+});
