cb4f6bdaa593df464e8e1780831cb6d2614042bf
Carlos Martos <40156600+CSharpFiasco@users.noreply.github.com>
2026-01-03 17:22:56 -0600

    update skills (#86)
    * update skills
    
    * update packages
    
    * rollback change to npm ci
    
    * change to list

--- src/features/skills/skills.ts
+++ src/features/skills/skills.ts
@@ -13,14 +13,14 @@ export class Skills {
     imageUrl: '/images/undraw_programming_re_kg9v.svg',
 
     skills: [
-      '⚡ Develop single page applications with Angular and React',
-      '⚡ Develop web applications with ASP.NET Core and Angular',
-      '⚡ Develop static generated websites with Astro',
-      '⚡ Astro deployment with Azure Static Web App',
-      '⚡ Relational database design',
-      '⚡ Refactor of legacy web apps to modern .NET API solutions with Angular front-end',
-      '⚡ Unit testing with xUnit and Jasmine',
-      '⚡ Azure DevOps and Github Actions builds and deployments to cloud and on-premise systems',
+      'Develop single page applications with Angular and React',
+      'Develop web applications with ASP.NET Core and Angular',
+      'Develop static generated websites with Astro',
+      'Astro deployment with Azure Static Web App',
+      'Relational database design',
+      'Refactor of legacy web apps to modern .NET API solutions with Angular front-end',
+      'Unit testing with xUnit and Jasmine',
+      'Azure DevOps and Github Actions builds and deployments to cloud and on-premise systems',
     ],
   } as const;
 }
