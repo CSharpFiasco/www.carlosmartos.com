51b3a62ade6e2dfbb304540aab6f9bb96f872e36
Carlos Martos <40156600+CSharpFiasco@users.noreply.github.com>
2026-04-12 10:09:27 -0500

    aesthetic changes

--- src/features/skills/skills.ts
+++ src/features/skills/skills.ts
@@ -11,16 +11,5 @@ export class Skills {
   protected readonly skillsSection = {
     firstWorkingDate: new Date(2017, 9, 1),
     imageUrl: '/images/undraw_programming_re_kg9v.svg',
-
-    skills: [
-      'Develop single page applications with Angular and React',
-      'Develop web applications with ASP.NET Core and Angular',
-      'Develop static generated websites with Astro',
-      'Astro deployment with Azure Static Web App',
-      'Relational database design',
-      'Refactor of legacy web apps to modern .NET API solutions with Angular front-end',
-      'Unit testing with xUnit and Jasmine',
-      'Azure DevOps and Github Actions builds and deployments to cloud and on-premise systems',
-    ],
   } as const;
 }
